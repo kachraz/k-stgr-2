2794655a9aa7d1a8166cb4afe12538f49b9299b9
kachraz <110726496+kachraz@users.noreply.github.com>
2024-10-29 17:39:51 +0000

    🫦

--- ST/t1/src/pag1.py
+++ ST/t1/src/pag1.py
@@ -1,8 +1,36 @@
 import streamlit as st
 
 
+def mark_down_test() -> None:
+    mktext = """
+# Testing out this as a variable 
+> Lets see if it can take it or not 
+1. Now we cbeck a number
+
+Now lets add able and see what it do 
+
+A | B | C
+---|---|---
+sniff | her | ass
+sniff | her | ass
+sniff | her | ass
+sniff | her | ass
+
+Everyday 
+"""
+    code = """
+import ass and pusy;
+print("Hello World");
+"""
+    st.markdown("# Page 1")
+    st.markdown(mktext)
+    st.code(code)
+
+
 def main_page_function() -> None:
     st.title("Page 1")
+    st.image("https://picsum.photos/200/300")
+    mark_down_test()
 
 
 # Execute Function
--- ST/t1/src/pag3.py
+++ ST/t1/src/pag3.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 def main_page_function() -> None:
     st.title("Page 3")
+    st.image("https://picsum.photos/200/300")
 
 
 # Execute Function
