ba7fd881837ad2074ec52a0df936c5641b4760ed
kachraz <110726496+kachraz@users.noreply.github.com>
2024-10-29 17:59:09 +0000

    🫦

--- ST/t1/src/pag2.py
+++ ST/t1/src/pag2.py
@@ -2,9 +2,19 @@ import streamlit as st
 from src.u import pic_func
 
 
+def hero1() -> None:
+    col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
+    with col1:
+        st.title("Page 2 - Hero Section")
+        pic_func()
+
+    with col2:
+        st.title("Page 2 - Hero Section")
+
+
 def main_page_function() -> None:
     st.title("Page 2")
-    pic_func()
+    hero1()
 
 
 # Execute Function
