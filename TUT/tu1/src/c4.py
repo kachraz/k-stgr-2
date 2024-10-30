555b796a66e05a610d90b47cf5588c554f4bcbcd
kachraz <110726496+kachraz@users.noreply.github.com>
2024-10-30 16:17:51 +0000

    🫦

--- TUT/tu1/src/c4.py
+++ TUT/tu1/src/c4.py
@@ -1,5 +1,20 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+# import matplotlib.pyplot as plt
+
+# Chart data here to be used in the functions
+c_d1 = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a", "b", "c"],
+)
+
+scatter_data = pd.DataFrame(
+    {
+        "x": np.random.randn(100),
+        "y": np.random.randn(100),
+    }
+)
 
 
 def B4() -> None:
@@ -7,6 +22,15 @@ def B4() -> None:
     st.divider()
 
     st.header("Chapter 4 - Chart Elements")
+
+    st.divider()
+    st.area_chart(c_d1)
+    st.bar_chart(c_d1)
+    st.line_chart(c_d1)
+
+    st.divider()
+    st.write("Scatter Plot")
+    st.scatter_chart(scatter_data)
 
 
 ################
