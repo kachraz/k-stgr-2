8d9f289d5bb6794830094c37e78b57b15bd55ebb
kachraz <110726496+kachraz@users.noreply.github.com>
2024-10-30 12:29:14 +0000

    🫦

--- TUT/tu1/panty.py
+++ TUT/tu1/panty.py
@@ -1,5 +1,51 @@
+import streamlit as st
+
+
+# Page Setup here
+def page_setup():
+    # Settting the page width as wide
+    st.set_page_config(layout="wide")
+
+    p1 = st.Page(
+        page="src/p1.py",
+        title="Page 1",
+        icon="👋",
+        default=True,
+    )
+
+    p2 = st.Page(
+        page="src/p2.py",
+        title="Page 2",
+        icon="👋",
+    )
+    p3 = st.Page(
+        page="src/p3.py",
+        title="Page 3",
+        icon="👋",
+    )
+
+    # -- Setting up the navigation ---
+    pg = st.navigation(
+        {
+            "ArmpitSniff": [p1],
+            "PantySniff": [p2, p3],
+        },
+    )
+
+    img1 = "https://i.ibb.co/9VXhzzP/image.png"
+    st.logo(
+        img1,
+    )
+    st.sidebar.warning(
+        "This is a sidebar",
+    )
+
+    pg.run()
+
+
+# Execute the main function with pages defined above
 def main():
-    print("Hello from tu1!")
+    page_setup()
 
 
 if __name__ == "__main__":
