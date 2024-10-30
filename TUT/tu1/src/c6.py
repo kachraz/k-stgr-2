63a300a07f96401d60f2a048bb8a346d4869a91d
kachraz <110726496+kachraz@users.noreply.github.com>
2024-10-30 19:09:00 +0000

    🫦

--- TUT/tu1/src/c6.py
+++ TUT/tu1/src/c6.py
@@ -16,6 +16,20 @@ def B6() -> None:
             st.warning("WARNING")
 
     st.divider()
+    "## Slider Function"
+    min_val, max_val = st.slider(
+        "Select a Range of Values",
+        0.0,
+        100.0,
+        (25.0, 75.0),
+    )
+    st.write("Values:", min_val, max_val)
+
+    # Another one
+    m_v = st.slider("Set Min Val", 0, 50, 25)
+    st.write("Min Val:", m_v)
+    sl_va = st.slider("Panty", m_v, 100, m_v)
+    st.write("Panty:", sl_va)
 
 
 ##################################
