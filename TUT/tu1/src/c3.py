1bfd9c15776db9f553f17238f310d05db8fb4eab
kachraz <110726496+kachraz@users.noreply.github.com>
2024-10-30 15:45:19 +0000

    🫦

--- TUT/tu1/src/c3.py
+++ TUT/tu1/src/c3.py
@@ -31,16 +31,32 @@ def B3() -> None:
     st.metric(label="Temperature", value="70 °F", delta="+1.2 °F")
 
     st.divider()
-    "## Charts"
-    st.bar_chart(df)
-    st.line_chart(df)
-    st.area_chart(df)
-    st.area_chart(edit_df)
 
     st.divider()
-    "## Maps"
+
+
+def B3Charts() -> None:
+    df = pd.DataFrame(
+        {
+            "first column": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+            "second column": [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+            "Names": ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+        }
+    )
+
+    "## Charts"
+    c1, c2, c3, c4 = st.columns(4)
+    with c1:
+        st.bar_chart(df)
+    with c2:
+        st.line_chart(df)
+    with c3:
+        st.area_chart(df)
+    with c4:
+        st.write("Testing charts here")
 
 
 #######################
 # Execution
 B3()
+B3Charts()
